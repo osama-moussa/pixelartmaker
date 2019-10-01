a01c3b3b08f06febd386d1c42c5f265e7a856cc0
osama moussa <osamamamoussa@gmail.com>
2019-10-01 02:06:10 +0200

    Update design.js

--- javascript/design.js
+++ javascript/design.js
@@ -1,25 +1,29 @@
-// Select color input
+// declared Dom elements variables 
 
 let canvas = document.getElementById('pixelCanvas');
 let size_picker = document.getElementById('sizePicker');
 let color =document.getElementById('colorPicker');
-// When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-  // Your code goes here!
+  // declare the height and width values as a variables
   let height = document.getElementById('inputHeight').value;
   let width = document.getElementById('inputWidth').value;
-// Select size input
+// looping to creat table dynamicly
   for (let i = 1; i <= height; i++) {
     let grid_row = document.createElement('tr');
     canvas.appendChild(grid_row);
     for (let x = 1; x <= width; x++) {
       let cells = document.createElement('td');
       grid_row.appendChild(cells);
-
+    // eventlistener to colour the cells with the selected colour
       cells.addEventListener('click', function() {
         this.style.backgroundColor = color.value;
       })
+      // eventlistener to clear the cell colour 
+      cells.addEventListener('dblclick', function() {
+        this.style.backgroundColor = 'transparent';
+      })
+    }
     }
    }
  }
